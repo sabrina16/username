ceb01a3059cdbd5992dabf7cb92dd87474656c7f
sabrina16 <sabrinatoubbeh@gmail.com>
2018-11-07 11:31:52 -0800

    working username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-# username.sh
+# username.sh by Sabrina Toubbeh
 echo "Enter a username: "
 read USERNAME
-while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
+while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid usesrname - 
-	3-12 lower case letters only
-	digits and underscore character!"
+	echo "You must enter a valid usesrname - The only characters that can be used are:
+        -lower case letters
+        -digits
+        -the underscore character!"
 	echo "Enter a username: "
 	read USERNAME
 done
