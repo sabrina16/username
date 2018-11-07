354f69dd1d29f2ce448498564c384afa3af0d12f
sabrina16 <sabrinatoubbeh@gmail.com>
2018-11-06 20:44:58 -0800

    final username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # username.sh
 echo "Enter a username: "
 read USERNAME
-while echo $USERNAME | egrep -v "^[a-z0-9]{3,12}$" > /dev/null 2>&1
+while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid usesrname - 
 	3-12 lower case letters only
